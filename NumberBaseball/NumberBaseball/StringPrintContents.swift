56785e8edcdd61fc38631b55d8cf3dd8d64bdc85
jaemuyeo <jemking94@gmail.com>
2021-05-28 23:25:36 +0900

    Refactor. 상수 삭제 및 추가

--- NumberBaseball/NumberBaseball/StringPrintContents.swift
+++ NumberBaseball/NumberBaseball/StringPrintContents.swift
@@ -8,10 +8,19 @@
 import Foundation
 
 struct StringPrintContents {
-    static let randomNumber = "임의의 수 : "
+    static let menu = """
+        1. 게임시작
+        2. 게임종료
+        """
+    static let selectMenu = "원하는 기능을 선택해주세요 :"
+    static let inputManual = """
+        숫자 3개를 띄어쓰기로 구분하여 입력해주세요.
+        중복 숫자는 허용하지 않습니다.
+        """
     static let strike = " 스트라이크, "
     static let ball = " 볼"
     static let win = "사용자승리!!"
     static let lose = "컴퓨터승리...!"
     static let chance = "남은 기회 : "
+    static let inputErrorMessage = "입력이 잘못되었습니다"
 }
